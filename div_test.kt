f578164961119c2234700e5733a99d7887b29fae
MaxGamess <141928465+MaxGamess@users.noreply.github.com>
2026-09-09 09:01:21 +0300

    Create div_test.kt

---
+++ div_test.kt
@@ -0,0 +1,11 @@
+fun main(){
+    var a = 17
+    var b = 5
+    val result1 = a.toDouble() / b
+    val result2 = a / b
+    val result3 = a % b
+    println("Числа $a и $b")
+    println("Обычное деление: $result1")
+    println("Целочисленное деление: $result2")
+    println("Остаток от деления: $result3")
+}
